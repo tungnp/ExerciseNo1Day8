080d27e4ed896b2c7cf91ccdc0af3c6161a6a996
tung nguyen <tungnp92@gmail.com>
2013-03-24 17:17:45 +0700

    1

--- ExerciseNo1Day8/ViewController.m
+++ ExerciseNo1Day8/ViewController.m
@@ -25,5 +25,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)pushSwitchButton:(UISwitch *)sender {
+    NSString* message;
+    if (sender.on) {
+        message = @"user switch button to ON";
+        UIImage* image = [UIImage imageNamed:@"troll.jpg"];
+        [self.imageView setImage:image];
+    }
+    else{
+        message = @"user switch button to OFF";
+        [self.imageView setImage:nil];
+    }
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"message"
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil, nil];
+    [alert show];
+}
 
 @end
